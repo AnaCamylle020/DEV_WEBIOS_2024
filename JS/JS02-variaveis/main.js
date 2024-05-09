7ac2f9bf848d08836a1d13fae68a82c4cc614168
AnaCamylle020 <bilinhanacamylle@gmail.com>
2024-05-08 21:46:20 -0300

    js/js02

---
+++ JS/JS02-variaveis/main.js
@@ -0,0 +1,60 @@
+//objeto console
+console.log("Hellow word!");
+//Quando trabalho com texto coloco mesmo dentro de aspas duplas ou simples 
+//variaveis
+//quero criar um espaço espaço na memoria do computador chamado_felippe e dizer que esse espaço na memoria ira receber o valor 8
+
+var _felippe = 8; 
+_felippe = "Messi"//redeclarar uma variavel
+//var teste//
+
+let letsGO = 4 // nao posso redeclarar//
+
+let letsGo =3
+
+console.log(_felippe);
+
+console.clear()
+//const//
+//PI = "5241"
+//const PI=//
+
+const PI =3.1415;
+
+console.log(PI);
+
+console.clear()
+
+let NAN = 0/0
+
+console.log (NAN)
+
+console.clear()
+
+let corinthians = "Melhor time,palmeiras sem mundial" //variavel recebeu uma string//
+
+console.log(corinthians)
+
+console.log( "Instituto" + "oportunidade" + "social");
+
+var numero1 = 2
+var texto   = "Ru tenho"
+var texto2  =  "Reais"
+
+console.log(texto + numero1  + texto2);
+//concatenar = é possivel concatenar strings ou concatenar variaveis//
+
+console.clear()
+
+//Valores Booleanos//
+
+var num1 = 5
+var num2 = 5
+//num1 é igual a num2//
+//1 é igual a 1 =true 
+
+console.log(num1 == num2)
+
+var teste;
+
+console.log(teste);
