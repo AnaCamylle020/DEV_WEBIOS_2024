779c60150f3ad15af16504c9e3043f9dc985cd91
AnaCamylle020 <bilinhanacamylle@gmail.com>
2024-05-09 21:43:31 -0300

    js02

--- JS/JS02-variaveis/main.js
+++ JS/JS02-variaveis/main.js
@@ -33,7 +33,7 @@ console.clear()
 
 let corinthians = "Melhor time,palmeiras sem mundial" //variavel recebeu uma string//
 
-console.log(corinthians)
+console.log(corinthians);
 
 console.log( "Instituto" + "oportunidade" + "social");
 
@@ -44,7 +44,7 @@ var texto2  =  "Reais"
 console.log(texto + numero1  + texto2);
 //concatenar = é possivel concatenar strings ou concatenar variaveis//
 
-console.clear()
+console.clear() //para apagar
 
 //Valores Booleanos//
 
@@ -57,4 +57,69 @@ console.log(num1 == num2)
 
 var teste;
 
-console.log(teste);
+console.log(teste)
+
+//operadores//
+
+var operador1 =8;
+var operador2 =null;
+
+console.log(operador1 * operador2); //8* null
+
+var operador3 ="5"
+var operador4 = 1
+
+console.log(operador3 - operador4); //"5" -1
+
+var operador5 ="5"
+var operador6 = 1
+
+console.log(operador5 + operador6);//"5" + 1 = 51
+
+var operador7 ="five"
+var operador8 =2
+
+console.log(operador7 * operador8); //five = 5 + 2
+
+
+console.clear()
+
+var valor1 = 2
+var valor2 = 2
+var resultado = valor1 % valor2
+
+console.log(resultado);
+
+console.clear()
+
+var compara = 2 > 3== 2//false false
+
+var teste   = !compara //true
+
+console.log(compara);
+//compara se é diferente ou igual
+//é diferente
+//se o valor é menor ou igual
+//só esta comparando o valor
+//compara o valor e o tipo
+ 
+//console.log("variavel compara" + compara)
+//console.log("variavel teste" + teste);
+  console.clear()
+
+let valor3 =4 //numerico
+let valor4 =6
+let resultado1 =valor3> valor4 && valor3 ==
+valor4
+//true e false ==false
+//true e true  == true
+//false e false == false
+
+ console.log(resultado1)
+
+ var valor5 = 5
+
+ console.log(typeof valor5)
+
+
+
