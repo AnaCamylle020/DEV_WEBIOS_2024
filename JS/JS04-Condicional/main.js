d4e05540fa6ec047949d775513b6a80beefb1096
AnaCamylle020 <bilinhanacamylle@gmail.com>
2024-05-14 21:41:05 -0300

    aula js

---
+++ JS/JS04-Condicional/main.js
@@ -0,0 +1,50 @@
+//if 
+//se todos os alunos estivessem com a blusa e crachá 
+//entao todos recebem
+var todosDeCracha=false
+//true==true>>true
+//treu==false>>false
+//if>>SE//else>>SENAO
+if(todosDeCracha == true){
+ console.log("todos recebem nota 10 em JS")
+}else{
+    console.log("todos recebem nota 0 em JS")
+}
+//exercício em sala//
+console.clear()
+
+var num1 =4
+var num2 =2
+//condiçao ? true : false
+num1> num2 ? console.log("numero1 é maior") :  console.log ("numero2 é menor") // abreviaçao - operador ternario 
+// if(numero1>numero2){//true
+//     console.log("variavel numero1 é maior que numero2")
+// }else{//false
+//    console.log("variavel numero1 é menor que variavel numeor2")
+// }
+console.clear()
+//se for de janeiro = Verão / junho = outubro / inverno
+
+//gerente/ estagio / CEO
+//Voce é gerente>> No perfil de gerente aparece todas as informações sobre estagiarios
+
+//Nome e senha
+//gerente / estagiario / CEO
+
+let mes = "JANEIRO"
+
+if(mes == "JANEIRO"){
+    console.log("É verão")
+}else if (mes == "JUNHO"){
+    console.log("É outono")
+}else if(mes == "OUTUBRO"){
+    console.log("É primavera")
+}else{
+    console.log("É inverno")
+}
+
+console.clear()
+
+//switch (mes)//guardo a expressão que quero validar
+//consegue fazer varias alterações ( else if - se não se)
+//
