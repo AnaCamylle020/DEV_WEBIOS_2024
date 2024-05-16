6a3fca51468b57ed32e2ff8a48367d4346e8620b
AnaCamylle020 <bilinhanacamylle@gmail.com>
2024-05-15 21:44:41 -0300

    aula JS05

---
+++ JS/JS05-Repetição/main.js
@@ -0,0 +1,40 @@
+//While
+//enquanto 0 <10 vou apresentar  a mensagem " 0ainda é menor que 10"
+
+var contador =0
+//0<=10
+
+// while(contador <=10){
+// console.log("0 ainda é menor que 10")
+// // debugger// é um ponot de parada
+// //contador = 3+1
+// //contador=4
+//  contador ++; // contador= contador + 1
+// }
+// do{
+//     debugger
+//     console.log (contador +"ainda é menor que 10")
+//     contador ++;
+// }while(contador<10)
+
+var contador = 10
+
+// while(conatador < 10)
+//  console.log(conatador +"ainda é menor que 10")
+// contador ++;//contador =contador + 1 
+
+//exemplo de lição de cas
+if{
+    for (){
+
+    }
+}else{
+    while(){
+
+    }
+}//
+
+
+for(var contador = 10; contador <  10 && contador >= 5 || contador == 90; contador++){
+    console.log(contador + " ainda é menor10")
+}
