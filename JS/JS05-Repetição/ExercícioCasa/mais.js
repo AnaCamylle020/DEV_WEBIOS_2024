95a9f8570ab98fac992e372278c4658b4c3465fd
AnaCamylle020 <bilinhaanacamylle@gmail.com>
2024-05-19 23:48:18 -0300

    update

---
+++ JS/JS05-Repetição/ExercícioCasa/mais.js
@@ -0,0 +1,11 @@
+var c = 0
+var tab=2
+
+if (tab == 3)
+   while (c<= 10 )
+        {console.log ("3×",c,"=",3*c)
+        c++}
+      else if (tab== 2)
+      while(c <= 10)
+        {console.log("2×",c,"=",2 * c)
+   c++};
