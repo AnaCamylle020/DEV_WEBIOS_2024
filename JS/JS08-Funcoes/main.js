5494dc06bb67bf719e38ceb7cc797153a713526d
AnaCamylle020 <bilinhaanacamylle@gmail.com>
2024-05-23 21:49:53 -0300

    Aula JS 08

---
+++ JS/JS08-Funcoes/main.js
@@ -0,0 +1,51 @@
+// funcao
+function soma(num1,num2,num3){ // parametros
+      var num1
+      var num2;
+      var num3;
+      return num1 + num2 + num3; 
+}
+ console.log(soma(2,3,2)) //<<recebe os valores que queremos somar //com
+//sem parenteses sao para passar os valores que queremos somar
+
+//console.log(soma)
+
+// if(e-mail++ "camylle.."&& senha: 123){
+//}return URL else
+
+//criei uma funçao que receba 4 valores e retone
+//a media desses valores
+console.clear()
+
+function media(nu1, nu2, nu3,nu4){
+   var nu1
+   var nu2;
+   var nu3;
+   var nu4;
+ return nu1 + nu2 +nu3 +nu4;
+}
+console.log(media(3,4,5,6))
+
+console.clear()
+
+
+const media2 = (n1, n2, n3, n4) => {
+
+    return( n1 + n2 + n3 + n4) / 4;
+}
+console.log(media2(5,5,5,5))
+
+console.clear()
+
+function onloadTela(){
+    alert("Sua tela esta carregada Bem-Vindo")
+   
+}
+console.log("Recarregou a tela")
+
+console.clear()
+
+const clique = () => {
+    alert("Bem-vindo voce clicou em mim")
+    console.log("Voce clicou em um botão")
+}
